49f7853ee7f5b878517da3edbcd46e310d3348cb
ajcarril <ajcarril@espol.edu.ec>
2021-06-28 00:41:24 -0500

    Actividad.java :D

---
+++ src/main/java/actividades/Actividad.java
@@ -0,0 +1,13 @@
+package actividades;
+import java.util.ArrayList;
+public class Actividad {
+    private int id;
+    private String fecha;
+    private String hora;
+    private String premio_1;
+    private String premio_2;
+    private String premio_3;
+    private int seleccionUsuario;
+    protected ArrayList <Participantes> listaParticipantes;
+    protected ArrayList <Participantes> listaGanadores;
+}
